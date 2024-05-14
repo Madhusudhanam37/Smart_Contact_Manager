1b08a80dcce073000b06fa01118331a417864a72
Madhusudhanam37 <162988213+Madhusudhanam37@users.noreply.github.com>
2024-05-14 14:23:17 +0530

    Delete Smart_Contact_Manager directory

--- Smart_Contact_Manager/src/main/java/com/smart_contact_manager/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smart_contact_manager.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.smart_contact_manager.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
